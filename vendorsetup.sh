94dc6130c1353d9995faded757526a51b98de8c5
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2023-05-26 15:53:16 +0800

    ngk braanch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
 export ALLOW_MISSING_DEPENDENCIES=true
-git clone https://github.com/alecchangod/NGK_android_kernel_xiaomi_sm8250 kernel/xiaomi/sm7250 -b test --depth=1
+git clone https://github.com/alecchangod/NGK_android_kernel_xiaomi_sm8250 kernel/xiaomi/sm7250 -b t-caf --depth=1
 git clone https://gitlab.com/alecchangod/trb_clang.git prebuilts/clang/host/linux-x86/clang-trb --depth=1 -b 17
