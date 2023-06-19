d8f678af781b253e09032f8605a9ec39609c634a
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2023-06-19 23:55:13 +0800

    ksu

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,9 @@
 export ALLOW_MISSING_DEPENDENCIES=true
 git clone https://github.com/alecchangod/kernel_xiaomi_sm7250 kernel/xiaomi/sm7250 -b ksu --depth=1
+ls -all
+free -h
 cd kernel/xiaomi/sm7250
-git submodule init && git submodule update KernelSU
+git config --global --add safe.directory /github/workspace/kernel/xiaomi/sm7250
+git submodule update --init KernelSU
 cd ../../..
 git clone https://gitlab.com/alecchangod/trb_clang.git prebuilts/clang/host/linux-x86/clang-trb --depth=1 -b 17
