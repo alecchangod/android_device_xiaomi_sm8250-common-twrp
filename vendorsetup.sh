5f1c14ecc604f29365470faaa723d35a543892a1
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2023-06-20 00:59:39 +0800

    hm...

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,5 +5,6 @@ free -h
 cd kernel/xiaomi/sm7250
 git config --global --add safe.directory /github/workspace/kernel/xiaomi/sm7250
 git submodule update --init KernelSU
+cd KernelSU && git checkout main && git pull && touch Android.mk && cd ..
 cd ../../..
 git clone https://gitlab.com/alecchangod/trb_clang.git prebuilts/clang/host/linux-x86/clang-trb --depth=1 -b 17
